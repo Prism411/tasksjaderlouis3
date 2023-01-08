c8b186d23403af1237ffc7a391e42e7ca89678bb
Jáder Louis <jaderlouis3@gmail.com>
2023-01-08 04:12:56 -0300

    Contract Services and Entities Fully Working

--- Project_ContractCompany/src/application/Program.java
+++ Project_ContractCompany/src/application/Program.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,25 +15,34 @@ import services.ServicesPayment;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NullPointerException {
 		
 	 Locale.setDefault(Locale.US);
 	 Scanner sc = new Scanner(System.in);
 
+		try {
 	 System.out.print("Quantos Contratos deseja acrescentar?");
 	 int n = sc.nextInt();
 	 for (int i = 0; i<n; i++) {
-		 System.out.println("Em quantos meses deseja terminar de pagar este contrato?");
+		 System.out.print("Em quantos meses deseja terminar de pagar este contrato?");
 		 int mes = sc.nextInt();
 		 System.out.println("Numero do contrato: ");
 		 int number = sc.nextInt();
-		 System.out.println("Preço do contrato: ");
+		 System.out.print("Preço do contrato: ");
 		 double price = sc.nextDouble();
 		 Contract c = new Contract(mes,number,price);
+		 System.out.println(c);
 		 
 	 }
 
+	 } catch (NullPointerException e) {
+		 System.out.println("VALOR NULO!!");
+		 e.printStackTrace();
 
+	 }catch (InputMismatchException e) {
+		 System.out.println("Digite um numero!");
+	
+	 }
+	}
 	}
 
-}
--- Project_ContractCompany/src/entities/Contract.java
+++ Project_ContractCompany/src/entities/Contract.java
@@ -4,13 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+import services.ServicesPayment;
 import services.ServicesTax;
 
 public class Contract {
 	
 	private DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private LocalDate start = LocalDate.now();
-	private LocalDate end;
+	private LocalDate end = start;
 	private int installments; // ESSAS SÃO AS PARCELAS 
 	private ServicesTax tax;
 	private int contractNumber;
@@ -31,17 +32,29 @@ public class Contract {
 	}
 	//-------------------------------------------- Getters
 
-	public Contract(int installments, int contractNumber, double contractValue) {
+	public Contract(int installments, int contractNumber, double contractValue) throws NullPointerException{
 		this.installments = installments;
 		this.contractNumber = contractNumber;
 		this.contractValue = contractValue;
+		
 
 	}
 	
-	public void ContractMaker() {
-		
-		end = end.plusMonths(installments);
-		
+	@SuppressWarnings("null")
+	public double ContractMaker() {
+		end = end.plusMonths(installments); //retorna a data final do contrato
+		 ServicesTax st = new ServicesPayment();
+		double ContractValueEnd = st.paypaltax(installments, contractValue);
+
+		return ContractValueEnd;
+	}
+	
+	@Override
+	
+	public String toString() {
+		ContractMaker();
+		return  "start=" + start.format(fmt) + ", end=" + end.format(fmt) + ", installments=" + installments
+				+", contractNumber=" + contractNumber + ", contractValue=" + contractValue + "ContractValueEnd =" + ContractMaker() + "]";
 	}
 	
 	
