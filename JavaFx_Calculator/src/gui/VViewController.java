7b4d2eb3101ff1b3563d7d051966bdaea282a397
Jáder Louis <jaderlouis3@gmail.com>
2023-01-20 19:16:34 -0300

    Calculator Working with labels

---
+++ JavaFx_Calculator/src/gui/VViewController.java
@@ -0,0 +1,32 @@
+package gui;
+
+import java.util.Locale;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class VViewController {
+	
+	@FXML
+	private TextField txtNumber1;
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	
+	public void onbtTestAction() {
+		Locale.setDefault(Locale.US);
+		double number1 = Double.parseDouble(txtNumber1.getText());
+		double number2 = Double.parseDouble(txtNumber2.getText());
+		double sum = number1 + number2 + 3232184320.5340;
+		labelResult.setText(String.format("%.2f", sum));
+		
+	}
+
+}
