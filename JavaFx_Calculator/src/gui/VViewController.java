910992c0c9dc7e59bf6d7805d5298a5ffc4f7160
Jáder Louis <jaderlouis3@gmail.com>
2023-01-20 20:13:38 -0300

    Calculador Done! (Only accepting doubles)

--- JavaFx_Calculator/src/gui/VViewController.java
+++ JavaFx_Calculator/src/gui/VViewController.java
@@ -1,15 +1,19 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class VViewController {
+public class VViewController implements Initializable {
 	
 	@FXML
 	private TextField txtNumber1;
@@ -22,10 +26,23 @@ public class VViewController {
 	
 	public void onbtTestAction() {
 		Locale.setDefault(Locale.US);
+		
+		try {
 		double number1 = Double.parseDouble(txtNumber1.getText());
 		double number2 = Double.parseDouble(txtNumber2.getText());
 		double sum = number1 + number2;
-		Alerts.showAlert("Resultado", null, String.format("%.2f", sum), AlertType.INFORMATION);
+		labelResult.setText(String.format("%.2f", sum));
+		} catch (Exception e) {
+			Alerts.showAlert("ERRO", null, "Excecao", AlertType.ERROR);
+		}
+		
+	}
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldDouble(txtNumber2);
 		
 	}
 
